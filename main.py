1ab95b44e4b15c6a1e8f916ffc067863186449bf
santakaws <brennonhahs@gmail.com>
2022-10-10 08:21:20 -0700

    Finished weather and main

---
+++ main.py
@@ -0,0 +1,38 @@
+import weather
+
+w_data = {}
+
+while True:
+    print('      *** TUFFY TITAN WEATHER LOGGER MAIN MENU \n\n')
+    print('1. Set data filename')
+    print('2. Add weather data')
+    print('3. Print daily report')
+    print('4. Print historical report')
+    print('5. Exit the program \n\n')
+    user_in = int(input('Enter menu choice: '))
+    print('\n')
+    
+    if user_in == 1:
+        filename = input('Enter data filename: ')
+        print('\n')
+        w_data = weather.read_data(filename=filename)
+    elif user_in == 2:
+        date = input('Enter date (YYYYMMDD): ')
+        time = input('Enter time (hhmmss): ')
+        temp = input('Enter temperature: ')
+        hum = input('Enter humidity: ')
+        rain = input('Enter rainfall: ')
+        w_data = {date+time:{'t':temp, 'h':hum, 'r':rain}}
+        weather.write_data(data=w_data, filename='w.dat')
+        print('\n')
+    elif user_in == 3:
+        date = input('Enter date (YYYYMMDD): ')
+        print('\n')
+        print(weather.report_daily(data=w_data, date=date))
+    elif user_in == 4:
+        print('\n')
+        print(weather.report_historical(data=w_data))
+    elif user_in == 5:
+        quit()
+    else:
+        user_in = input('Invalid choice. Re-enter menu choice: ')
---
+++ weather.py
@@ -0,0 +1,97 @@
+import json
+import calendar
+
+def read_data(filename):
+    try:
+        with open (filename, 'r') as f:
+            return json.load(f)
+    except:
+        return {}
+
+def write_data(data, filename):
+    with open(filename, 'w') as f:
+        f.write(json.dumps(data))
+
+def max_temperature(data, date):
+    x = -200
+    for key in data:
+        if key[0:8] == date and data[key]['t'] > x:
+            x = data[key]['t']
+    return x
+
+def min_temperature(data, date):
+    x = 200
+    for key in data:
+        if key[0:8] == date and data[key]['t'] < x:
+            x = data[key]['t']
+    return x
+
+def max_humidity(data, date):
+    x = 0
+    for key in data:
+        if key[0:8] == date and data[key]['h'] > x:
+            x = data[key]['h']
+    return x
+
+def min_humidity(data, date):
+    x = 100
+    for key in data:
+        if key[0:8] == date and data[key]['h'] < x:
+            x = data[key]['h']
+    return x
+
+def tot_rain(data, date):
+    sum_rain = 0
+    for key in data:
+        if key[0:8] == date:
+            sum_rain += data[key]['r']
+    return sum_rain
+
+def report_daily(data, date):
+    display = "========================= DAILY REPORT ========================\n"
+    display += 'Date                      Time  Temperature  Humidity  Rainfall\n'
+    display += '====================  ========  ===========  ========  ========\n'
+
+    for key in data:
+        if key[0:8] == date:
+            m = calendar.month_name[int(date[4:6])]
+            d = ' ' + str(int(date[6:8])) + ', '
+            y = str(int(date[0:4]))
+            mdy = m+d+y
+
+            tm = key[8:10] + ':' + key[10:12] + ':' + key[12:14]
+            t = data[key]['t']
+            h = data[key]['h']
+            r = data[key]['r']
+            display = display + f'{mdy: <22}' + f'{tm: <10}' + f'{t: >11}' + f'{h: >10}' + f'{r: >10}\n'
+    
+    return display
+
+def report_historical(data):
+    display =  '============================== HISTORICAL REPORT ===========================\n'
+    display += '                          Minimum      Maximum   Minumum   Maximum     Total\n'
+    display += 'Date                  Temperature  Temperature  Humidity  Humidity  Rainfall\n'
+    display += '====================  ===========  ===========  ========  ========  ========\n'
+    key_list = []
+
+    for key in data:
+        if key[0:8] not in key_list:
+            key_list.append(key[0:8])
+            m = calendar.month_name[int(key[4:6])]
+            d = ' ' + str(int(key[6:8])) + ', '
+            y = str(int(key[0:4]))
+            mdy = m+d+y
+            mint = min_temperature(data=data, date=key[0:8])
+            maxt = max_temperature(data=data, date=key[0:8])
+            minh = min_humidity(data=data, date=key[0:8])
+            maxh = max_humidity(data=data, date=key[0:8])
+            totr = tot_rain(data=data, date=key[0:8])
+            totrc = f'{totr:.2f}'
+            display += f'{mdy: <20}{mint: >13}{maxt: >13}{minh: >10}{maxh: >10}{totrc: >10}\n'
+    
+    return display
+        
+
+
+
+
